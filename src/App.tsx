71f7962b8e4b88f5d81c3c139ef9c8baf7978e3c
alecbrando <37994059+alecbrando@users.noreply.github.com>
2021-04-16 17:32:39 -0700

    initial working on video player

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Video from "./components/Video";
+import styled from "styled-components";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <AppVideos>
+        <Video />
+        <Video />
+      </AppVideos>
+    </AppContainer>
   );
 }
 
 export default App;
+
+const AppContainer = styled.div`
+  height: 100vh;
+  background-color: black;
+  display: grid;
+  place-items: center;
+`;
+const AppVideos = styled.div`
+  position: relative;
+  border-radius: 20px;
+  overflow: scroll;
+  height: 800px;
+  max-width: 500px;
+  width: 80%;
+  background-color: white;
+  scroll-snap-type: y mandatory;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  --ms-overflow-style: none;
+  scrollbar-width: none;
+`;
